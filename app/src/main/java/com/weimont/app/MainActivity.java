858d85cdca03825647ecd31d840f22fcfe503efb
unknown <montespierre@gmail.com>
2021-02-08 22:09:42 -0500

    lector

--- app/src/main/java/com/weimont/app/MainActivity.java
+++ app/src/main/java/com/weimont/app/MainActivity.java
@@ -1,14 +1,56 @@
 package com.weimont.app;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnScanner;
+    private TextView tvBarCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnScanner = findViewById(R.id.btnScanner);
+        tvBarCode = findViewById(R.id.tvBarCode);
+
+        btnScanner.setOnClickListener(nonClickListener);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null){
+            if(result.getContents() != null){
+                tvBarCode.setText("El codigo de barras es:\n" + result.getContents());
+            }else{
+                tvBarCode.setText("Error al escanear");
+            }
+        }
+    }
+
+    private View.OnClickListener nonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnScanner:
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                    break;
+            }
+        }
+    };
+
 }
